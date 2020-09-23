508b8d9462be51f70d02772043625cfa36421372
eyilmaz4 <eyilmaz4@na.edu>
2020-09-23 15:37:47 -0500

    EYilmaz

--- src/test/java/TestClasses/Micro_22_September.java
+++ src/test/java/TestClasses/Micro_22_September.java
@@ -20,26 +20,26 @@ public class Micro_22_September {
     public static void setup(){
          WebDriverManager.chromedriver().setup();
         driver= new ChromeDriver();
-        driver.get("https://the-internet.herokuapp.com/");
+        //driver.get("https://the-internet.herokuapp.com/");
         driver.manage().deleteAllCookies();
-        driver.manage().timeouts().implicitlyWait(1000, TimeUnit.SECONDS);
         jse=(JavascriptExecutor) driver;
-        jse.executeScript("window.location='https://the-internet.herokuapp.com/");
+        jse.executeScript("window.location=arguments[0]","https://the-internet.herokuapp.com/ ");
 
     }
+
+    /**
+     * This test is for Scroll Down/Up
+     * @throws InterruptedException
+     */
     @Test
     public void test1() throws InterruptedException {
         WebElement element=driver.findElement(By.xpath("//*[text()='Infinite Scroll']"));
         element.click();
-        Thread.sleep(10000);
+        Thread.sleep(1000);
         jse.executeScript("window.scrollBy(1,1800)");//scroll down, if you want up give negative value
         Thread.sleep(5000);
         jse.executeScript("window.scrollBy(1,-1800)");
 
-        /**
-        WebElement showMore=driver.findElement(By.xpath("//a[text()='Online Banking Service Agreement']"));
-        WebElement agree=(WebElement)jse.executeScript("return document.getElementById('footer_bofa_online_banking_service_agreement')");
-        jse.executeScript("arguments[0].scrollIntoView(true)",showMore); */
 
     }
 
